4bfecbc4d78bb3452a2e8ce26a36fb1a515e2b18
landisal <landisal@mail.uc.edu>
2022-09-26 19:59:02 -0400

    Finished

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        double costSpring = 240.23;
+        double costSummer = 185.34;
+        double costFall = 189.58;
+        double costWinter = 428.94;
+        double totalCost = costFall + costSpring + costWinter + costFall;
+
+        System.out.println("Your Maintenance cost  for Spring is " +costSpring);
+        System.out.println("Your Maintenance cost  for Summer is " +costSummer);
+        System.out.println("Your Maintenance cost  for Fall is " +costFall);
+        System.out.println("Your Maintenance cost  for Winter is " +costWinter);
+        System.out.println("Your total Maintenance cost is  " +totalCost);
     }
 }
